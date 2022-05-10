65af81bc507fa5696486e64f193e9e2e95cc75ba
Gil Kapel <gil.kapel@gmail.com>
2022-05-11 00:11:53 +0300

    1

--- code/part3.py
+++ code/part3.py
@@ -2,12 +2,152 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import copy
+from utils import video_to_frames
+
+
+def poisson_noisy_image(orig_img, a):
+    """
+    Creates a Poisson noisy image.
+    :param orig_img: The Original image. np array of size [H x W] and of type uint8.
+    :param a: number of photons scalar factor
+    :return:
+    noisy_img: The noisy image. np array of size [H x W] and of type uint8.
+    """
+    # ====== YOUR CODE: ======
+    # ========================
+    photons_in_img = copy.deepcopy(orig_img).astype('float') * a
+    poisson_photons_img = np.random.poisson(photons_in_img)
+    poisson_img = poisson_photons_img / a
+    noisy_img = np.clip(poisson_img, 0, 255).astype('uint8')
+    return noisy_img
+
+
+def double_conv(X, d_kernel, x_shape):
+    first_conv = cv2.filter2D(np.reshape(X, x_shape, order='F'), -1, d_kernel)
+    second_conv = cv2.filter2D(first_conv, -1, d_kernel)
+    return second_conv.flatten('F')
+
+
+def denoise_by_l2(Y, X, num_iter, lambda_reg, epsilon0=0):
+    """
+    L2 image denoising.
+    :param Y: The noisy image. np array of size [H x W]
+    :param X: The Original image. np array of size [H x W]
+    :param num_iter: the number of iterations for the algorithm perform
+    :param lambda_reg: the regularization parameter
+    :return:
+    Xout: The restored image. np array of size [H x W]
+    Err1: The error between Xk at every iteration and Y.
+    np array of size [num_iter]
+    Err2: The error between Xk at every iteration and X.
+    np array of size [num_iter]
+    """
+    # ====== YOUR CODE: ======
+    # ========================
+    d_kernel = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    x_shape = X.shape
+    Y = Y.flatten('F')
+    X = X.flatten('F')
+    Xout = Y
+    Err1 = np.zeros(num_iter)
+    Err2 = np.zeros(num_iter)
+    for k in range(num_iter):
+        double_conv_x_k = double_conv(Xout, d_kernel, x_shape)
+        G_k = Xout + lambda_reg * double_conv_x_k - Y
+        double_conv_g_k = double_conv(G_k, d_kernel, x_shape)
+        mu = (G_k.transpose() @ G_k) / (G_k.transpose() @ G_k + lambda_reg * G_k.transpose() @ double_conv_g_k)
+        Xout = Xout - mu * G_k
+        conv_X_out = (cv2.filter2D(np.reshape(Xout, x_shape, order='F'), -1, d_kernel)).flatten('F')
+        Err1[k] = (Xout - Y).transpose() @ (Xout - Y) + lambda_reg * (conv_X_out.transpose() @ conv_X_out)
+        Err2[k] = (Xout - X).transpose() @ (Xout - X)
+    Xout = np.reshape(Xout, x_shape, order='F').astype('uint8')
+    return Xout, Err1, Err2
+
+
+def denoise_by_tv(Y, X, num_iter, lambda_reg, epsilon0):
+    """
+    TV image denoising.
+    :param Y: The noisy image. np array of size [H x W]
+    :param X: The Original image. np array of size [H x W]
+    :param num_iter: the number of iterations for the algorithm perform
+    :param lambda_reg: the regularization parameter
+    :param: epsilon0: small scalar for numerical stability
+    :return:
+    Xout: The restored image. np array of size [H x W]
+    Err1: The error between Xk at every iteration and Y.
+    np array of size [num_iter]
+    Err2: The error between Xk at every iteration and X.
+    np array of size [num_iter]
+    """
+    # ====== YOUR CODE: ======
+    # ========================
+    Xout = Y
+    mu = 150 * epsilon0
+    Err1 = np.zeros(num_iter)
+    Err2 = np.zeros(num_iter)
+    for k in range(num_iter):
+        x_grad = np.gradient(Xout)
+        normal_x_grad = x_grad / np.sqrt(np.power(x_grad[0], 2) + np.power(x_grad[1], 2) + np.power(epsilon0, 2))
+        x_divergence = np.ufunc.reduce(np.add, [np.gradient(normal_x_grad[i], axis=i) for i in range(2)])
+        U_k = 2 * (Y - Xout) + lambda_reg * x_divergence
+        Xout = Xout + mu * U_k / 2
+        Xout_grad = np.sqrt(np.power(np.gradient(Xout)[0], 2) + np.power(np.gradient(Xout)[1], 2))
+        TV = np.sum(Xout_grad)
+        Err1[k] = (Xout.flatten('F') - Y.flatten('F')).transpose() @ (Xout.flatten('F') - Y.flatten('F')) + lambda_reg * TV
+        Err2[k] = (Xout.flatten('F') - X.flatten('F')).transpose() @ (Xout.flatten('F') - X.flatten('F'))
+    Xout = Xout.astype('uint8')
+    return Xout, Err1, Err2
+
+
+def make_restoration_and_error_graph(noisy_img, resized_red_image, denoise_func, num_iter, lambda_reg, epsilon0):
+    noisy_img = copy.deepcopy(noisy_img)
+    resized_red_image = copy.deepcopy(resized_red_image)
+    Xout, Err1, Err2 = denoise_func(noisy_img, resized_red_image, num_iter,
+                                    lambda_reg, epsilon0)
+    n = np.linspace(0, num_iter, num_iter)
+    plt.plot(n, np.log2(1 + Err1), 'r', label='Err1')
+    plt.plot(n, np.log2(1 + Err2), 'b', label='Err2')
+    plt.legend()
+    plt.xlabel('number of epochs')
+    plt.ylabel('error rate - log scale')
+    cv2.imshow("restoration image", Xout)
+    plt.show()
 
 
 def main():
-    pass
+    #   section 3.a
+    frames = video_to_frames('../given_data/Flash Gordon Trailer.mp4', 20, 21)
+    bgr = frames[0]
+    cv2.imshow("20 second's frame", bgr)
+    red_channel = bgr[:, :, 2]
+    cv2.imshow("red channel", red_channel)
+    dim = (int(red_channel.shape[1] / 2), int(red_channel.shape[0] / 2))
+    resized_red_image = cv2.resize(red_channel, dim)
+    cv2.imshow("resized_red_image", resized_red_image)
+    noisy_img = poisson_noisy_image(resized_red_image, 1/3)
+    cv2.destroyAllWindows()
+    cv2.imshow("noisy_img", noisy_img)
+
+    # section 3.b
+    make_restoration_and_error_graph(noisy_img, resized_red_image, denoise_by_l2, 50, 0.5, 0)
+
+    # section 3.c
+    make_restoration_and_error_graph(noisy_img, resized_red_image, denoise_by_tv, 200, 10, 2e-4)
+
+    #   section 3.d
+    #   section 3.e
+    frames = video_to_frames('../given_data/Flash Gordon Trailer.mp4', 38, 39)
+    bgr = frames[3]
+    red_channel = bgr[:, :, 2]
+    dim = (int(red_channel.shape[1] / 2), int(red_channel.shape[0] / 2))
+    resized_red_image = cv2.resize(red_channel, dim)
+    cv2.imshow("resized_red_image", resized_red_image)
+    noisy_img = poisson_noisy_image(resized_red_image, 3)
+    cv2.imshow("noisy_img", noisy_img)
+    make_restoration_and_error_graph(noisy_img, resized_red_image, denoise_by_l2, 50, 0.5, 0)
+    make_restoration_and_error_graph(noisy_img, resized_red_image, denoise_by_tv, 200, 20, 2e-4)
 
 
 if __name__ == '__main__':
     main()
-
+    cv2.waitKey(0)
--- code/utils.py
+++ code/utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import copy
 
 
 def show_histogram(frame, title: str, x_label: str, y_label: str):
@@ -14,3 +13,64 @@ def show_histogram(frame, title: str, x_label: str, y_label: str):
     plt.title(title)
     plt.show(block=True)
 
+
+def video_to_frames(vid_path: str, start_second, end_second):
+    """
+    Load a video and return its frames from the wanted time range.
+    :param vid_path: video file path.
+    :param start_second: time of first frame to be taken from the
+    video in seconds.
+    :param end_second: time of last frame to be taken from the
+    video in seconds.
+    :return:
+    frame_set: a 4D uint8 np array of size [num_of_frames x H x W x C]
+    containing the wanted video frames.
+    """
+    # ====== YOUR CODE: ======
+    # ========================
+    cap = cv2.VideoCapture(vid_path)
+    frame_set = []
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    video_time = cap.get(cv2.CAP_PROP_FRAME_COUNT) / fps
+    i = 0  # iterator to iterate one frame each loop
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if start_second * fps > i:  # continue until you get to the required start second
+            i += 1
+            continue
+        if end_second * fps < i:  # stop when you get to the required end second
+            if end_second == start_second and end_second > 0:
+                frame_set.append(cv2.cvtColor(frame, None))
+            break
+        if not ret or video_time < end_second:
+            print('wrong input')
+            return
+        frame_set.append(cv2.cvtColor(frame, None))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        i += 1
+    cap.release()
+    cv2.destroyAllWindows()
+    return np.array(frame_set)
+
+
+def match_corr(corr_obj, img):
+    """
+    return the center coordinates of the location of 'corr_obj' in 'img'.
+    :param corr_obj: 2D numpy array of size [H_obj x W_obj]
+    containing an image of a component.
+    :param img: 2D numpy array of size [H_img x W_img]
+    where H_img >= H_obj and W_img>=W_obj,
+    containing an image with the 'corr_obj' component in it.
+    :return:
+    match_coord: the two center coordinates in 'img'
+    of the 'corr_obj' component.
+    """
+    # ====== YOUR CODE: ======
+    object_conv = cv2.filter2D(corr_obj, -1, corr_obj, borderType=cv2.BORDER_CONSTANT)
+    object_max = object_conv.max()
+    image_corr = cv2.filter2D(img, -1, corr_obj, borderType=cv2.BORDER_CONSTANT)
+    match_coord = np.unravel_index((np.abs(image_corr - object_max)).argmin(), image_corr.shape)
+    # ========================
+
+    return match_coord
